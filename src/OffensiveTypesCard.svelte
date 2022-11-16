7e27c5808e42ff7294fa1cf4de4b1aa792c800f1
Michael Matthews <michael.64.matthews@gmail.com>
2022-11-15 20:14:43 -0500

    Add OffensiveTypes card

---
+++ src/OffensiveTypesCard.svelte
@@ -0,0 +1,55 @@
+<script>
+    /**
+     * This card is meant for quizzing on offensive typing
+    */
+    import { Button } from "carbon-components-svelte";
+
+    import SingleChoiceCard from "./SingleChoiceCard.svelte";
+    import { TYPES, TYPE_CHART } from "./types";
+    
+
+    /**
+     * type <string> - one of the types, lowercase.  enumerated in types.js
+     */
+    export let type = undefined
+
+    /**
+     * currently only supports 0.5 and 2x (resistant/weak)
+     */
+    export let effectiveness = 0.5
+
+    const options = TYPES;
+    let answers;
+
+    $: effect = effectiveness == 0.5 ? 'not very effective' : `super effective`
+    $: if (!type) {
+        type = chooseRandomType();
+    }
+    $: if (type && effectiveness) {
+        answers = Object.entries(TYPE_CHART[type]).map((entry) => {
+            const [defenderType, effective] = entry;
+            if (effective == effectiveness) {
+                return defenderType;
+            }
+        }).filter ((element) => !!element)
+    }
+
+    function refreshCard () {
+        type = chooseRandomType();
+    }
+
+    function chooseRandomType () {
+        const randIndex = Math.floor(Math.random()*TYPES.length);
+        return TYPES[randIndex];
+    }
+
+</script>
+
+
+<SingleChoiceCard
+    question={`Which of the following is ${type} ${effect} against?`}
+    {options}
+    {answers}
+    on:refresh={refreshCard}
+    >
+</SingleChoiceCard>
