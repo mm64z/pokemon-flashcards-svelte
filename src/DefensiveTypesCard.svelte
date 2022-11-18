ba3e5a7a9bccd3416da63cce1712fad81fcacc8c
Michael Matthews <michael.64.matthews@gmail.com>
2022-11-17 19:10:09 -0500

    Switch to 2 space tabs instead of 4

--- src/DefensiveTypesCard.svelte
+++ src/DefensiveTypesCard.svelte
@@ -1,55 +1,53 @@
 <script>
-    /**
-     * This card is meant for quizzing on defensive typing
-    */
-    import { Button } from "carbon-components-svelte";
+  /**
+   * This card is meant for quizzing on defensive typing
+   */
+  import { Button } from "carbon-components-svelte";
 
-    import SingleChoiceCard from "./SingleChoiceCard.svelte";
-    import { TYPES, TYPE_CHART } from "./types";
-    
+  import SingleChoiceCard from "./SingleChoiceCard.svelte";
+  import { TYPES, TYPE_CHART } from "./types";
 
-    /**
-     * type <string> - one of the types, lowercase.  enumerated in types.js
-     */
-    export let type = undefined
+  /**
+   * type <string> - one of the types, lowercase.  enumerated in types.js
+   */
+  export let type = undefined;
 
-    /**
-     * currently only supports 0.5 and 2x (resistant/weak)
-     */
-    export let effectiveness = 0.5
+  /**
+   * currently only supports 0.5 and 2x (resistant/weak)
+   */
+  export let effectiveness = 0.5;
 
-    const options = TYPES;
-    let answers;
+  const options = TYPES;
+  let answers;
 
-    $: effect = effectiveness == 0.5 ? 'not very effective' : `super effective`
-    $: if (!type) {
-        type = chooseRandomType();
-    }
-    $: if (type && effectiveness) {
-        answers = Object.entries(TYPE_CHART).map((entry) => {
-            const [attackerType, defenderEntries] = entry;
-            if (defenderEntries[type] == effectiveness) {
-                return attackerType;
-            }
-        }).filter ((element) => !!element)
-    }
+  $: effect = effectiveness == 0.5 ? "not very effective" : `super effective`;
+  $: if (!type) {
+    type = chooseRandomType();
+  }
+  $: if (type && effectiveness) {
+    answers = Object.entries(TYPE_CHART)
+      .map((entry) => {
+        const [attackerType, defenderEntries] = entry;
+        if (defenderEntries[type] == effectiveness) {
+          return attackerType;
+        }
+      })
+      .filter((element) => !!element);
+  }
 
-    function refreshCard () {
-        type = chooseRandomType();
-    }
+  function refreshCard() {
+    type = chooseRandomType();
+  }
 
-    function chooseRandomType () {
-        const randIndex = Math.floor(Math.random()*TYPES.length);
-        return TYPES[randIndex];
-    }
-
+  function chooseRandomType() {
+    const randIndex = Math.floor(Math.random() * TYPES.length);
+    return TYPES[randIndex];
+  }
 </script>
 
-
 <SingleChoiceCard
-    question={`Which of the following is ${effect} against ${type}?`}
-    {options}
-    {answers}
-    on:refresh={refreshCard}
-    >
-</SingleChoiceCard>
+  question={`Which of the following is ${effect} against ${type}?`}
+  {options}
+  {answers}
+  on:refresh={refreshCard}
+/>
--- src/SingleChoiceCard.svelte
+++ src/SingleChoiceCard.svelte
@@ -1,61 +1,57 @@
 <script>
-    import { Button, MultiSelect, Select, Tile } from "carbon-components-svelte";
-	import { createEventDispatcher } from 'svelte';
+  import { Button, MultiSelect, Select, Tile } from "carbon-components-svelte";
+  import { createEventDispatcher } from "svelte";
 
-	const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-    export let question;
-    export let options;
-    export let answers;
+  export let question;
+  export let options;
+  export let answers;
 
-    let flipped = false;
+  let flipped = false;
 
-    $: if (question) {
-        flipped = false
-    }
+  $: if (question) {
+    flipped = false;
+  }
 
-    function handleFlip() {
-        flipped = !flipped
-    }
+  function handleFlip() {
+    flipped = !flipped;
+  }
 
-    function refreshCard() {
-        dispatch('refresh')
-    }
-
+  function refreshCard() {
+    dispatch("refresh");
+  }
 </script>
 
-<div class='card'>
-    <div class='question'>
-        {question}
+<div class="card">
+  <slot name="icon" />
+  <div class="question">
+    {question}
+  </div>
+  {#if !flipped}
+    <div class="front" on:click={handleFlip} on:keypress={handleFlip}>
+      Options:
+      <div>
+        {options}
+      </div>
     </div>
-    {#if !flipped}
-    <div class='front' on:click={handleFlip} on:keypress={handleFlip}>
-        Options:
-        <div>
-            {options}
-        </div>
-
+  {:else}
+    <div class="back" on:click={handleFlip} on:keypress={handleFlip}>
+      Answer:
+      <div class="options">
+        {answers}
+      </div>
     </div>
-    {:else}
-    <div class='back' on:click={handleFlip} on:keypress={handleFlip}>
-        Answer:
-        <div class='options'>
-            {answers}
-        </div>
-    </div>
-    {/if}
+  {/if}
 </div>
-<Button on:click={refreshCard}>
-    Refresh
-</Button>
+<Button on:click={refreshCard}>Refresh</Button>
 
 <style>
-    .card {
-        width: 800px;
-        box-shadow: 3px 3px 3px 0;
-        border: 3px solid;
-        border-color: black;
-        margin: 10px;
-    }
-
+  .card {
+    width: 800px;
+    box-shadow: 3px 3px 3px 0;
+    border: 3px solid;
+    border-color: black;
+    margin: 10px;
+  }
 </style>
